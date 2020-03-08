a2d6030f98b6159e206713b5abafe9b72e66e0fa
rqcao <rqcao@outlook.com>
2020-03-08 14:28:26 +0800

    完成tabbar图标

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -4,26 +4,26 @@ Component({
     color: "#7A7E83",
     selectedColor: "#3cc51f",
     list: [{
-      pagePath: "pages/daily/daily",
-      iconPath: "/image/icon_component.png",
-      selectedIconPath: "/image/icon_component_HL.png",
+      pagePath: "/pages/daily/daily",
+      iconPath: "/image/day.png",
+      selectedIconPath: "/image/day_HL.png",
       text: "每日一诗"
     }, {
-      pagePath: "pages/logs/log",
-      iconPath: "/image/icon_API.png",
-      selectedIconPath: "/image/icon_API_HL.png",
+      pagePath: "/pages/explore/explore",
+      iconPath: "/image/explore.png",
+      selectedIconPath: "/image/explore_HL.png",
       text: "探索好诗"
-      }, {
-        pagePath: "/index/index2",
-        iconPath: "/image/icon_API.png",
-        selectedIconPath: "/image/icon_API_HL.png",
-        text: "文库大全"
-      }, {
-        pagePath: "/index/index2",
-        iconPath: "/image/icon_API.png",
-        selectedIconPath: "/image/icon_API_HL.png",
-        text: "酷功能"
-      }]
+    }, {
+      pagePath: "/pages/library/library",
+      iconPath: "/image/library.png",
+      selectedIconPath: "/image/library_HL.png",
+      text: "文库大全"
+    }, {
+      pagePath: "/pages/cool/cool",
+      iconPath: "/image/cool.png",
+      selectedIconPath: "/image/cool_HL.png",
+      text: "酷功能"
+    }]
   },
   attached() {
   },
